5ef63d23f7110e234e991a92cb8fc9805f046a31
Alvin Yap <leaf.alvin@gmail.com>
2023-10-31 22:48:06 +0800

    feat: add nav bar

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,5 +1,20 @@
+import { faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div>
+        <Link href="/">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-default-text">leaf.alvin@gmail.com</p>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
